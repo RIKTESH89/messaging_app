9651c4590008d769f05d38e878eec943dcd7cbc2
RIKTESH89 <rikteshsingh28801@gmail.com>
2024-06-14 12:54:08 +0530

    minor change

--- frontend/app/startChat/page.jsx
+++ frontend/app/startChat/page.jsx
@@ -186,6 +186,7 @@ export default function StartChat() {
     const [socket, setSocket] = useState(null);
     const searchParams = useSearchParams();
     const recieverId = searchParams.get('recieverId')
+    const username = searchParams.get('user');
     const [sender, setSender] = useRecoilState(senderIdAtom)
     const { data: session, status } = useSession();
     const router = useRouter();
@@ -258,7 +259,7 @@ export default function StartChat() {
                     <div className="w-screen flex flex-row justify-between p-4">
                         <div className="flex items-center space-x-3 rtl:space-x-reverse">
                             <svg className="absolute w-12 h-12 text-gray-400 -left-1 relative w-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
-                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{searchParams.get('user')}</span>
+                            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{username}</span>
                         </div>
                         <div className="flex flex-row items-center justify-end  dark:text-white mr-4">
                             <button onClick={() => { router.push("/chatting") }}>
@@ -305,12 +306,12 @@ export default function StartChat() {
             <div className="flex items-center px-1.5 py-2 bg-gray-50 dark:bg-gray-700 fixed inset-x-0 bottom-0 p-10">
                 <input className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setselfmsg(e.target.value)} value={seflmsg} type="text" placeholder="message" />
                 <button className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600" onClick={async () => {
-                    if (searchParams.get('recieverId') && session?.user?.id) {
-                        const msgId = await msgCreate(session.user.id, searchParams.get('recieverId'), seflmsg);
-                        socket?.send(JSON.stringify({ type: "message", sid: parseInt(session.user.id), rid: parseInt(searchParams.get('recieverId')), text: seflmsg }));
+                    if (recieverId && session?.user?.id) {
+                        const msgId = await msgCreate(session.user.id, recieverId, seflmsg);
+                        socket?.send(JSON.stringify({ type: "message", sid: parseInt(session.user.id), rid: parseInt(recieverId), text: seflmsg }));
                     }
 
-                    setchathistory((prevHistory) => [...prevHistory, { id: "", senderId: session.user.id, body: seflmsg, recieverId: searchParams.get('recieverId'), createdAt: gettime() }]);
+                    setchathistory((prevHistory) => [...prevHistory, { id: "", senderId: session.user.id, body: seflmsg, recieverId: searchParams, createdAt: gettime() }]);
                     setselfmsg("");
 
                 }}><svg className="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
