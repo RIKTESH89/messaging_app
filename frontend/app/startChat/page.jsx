2f8a81d342c62a68ed80dc4347135ea36be174d7
RIKTESH89 <rikteshsingh28801@gmail.com>
2024-06-14 11:10:38 +0530

    deploying frontend 3

--- frontend/app/startChat/page.jsx
+++ frontend/app/startChat/page.jsx
@@ -48,7 +48,7 @@ export default function startChat(){
 
     useEffect(() => {
         if(sender){
-            const newSocket = new WebSocket('ws://localhost:8080');
+            const newSocket = new WebSocket('wss://messaging-app-idpa.onrender.com');
         newSocket.onopen = () => {
             console.log("sockets are open now")
             console.log(chatState[searchParams.get('recieverId')])
@@ -148,12 +148,12 @@ export default function startChat(){
         <button class="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600" onClick={async function(){
             
             if(searchParams.get('recieverId') && session?.user?.id){
-                setselfmsg("");
                 const msgId = await msgCreate(session.user.id,searchParams.get('recieverId'),seflmsg);                
                 socket?.send(JSON.stringify({type:"message",sid : parseInt(session.user.id),rid:parseInt(searchParams.get('recieverId')),text:seflmsg}));
                 }
                 
                 setchathistory((prevHistory) => [...prevHistory, {id:"",senderId:session.user.id,body:seflmsg,recieverId:searchParams.get('recieverId'),createdAt:gettime()}]); 
+                setselfmsg("");
                 
                 }}><svg class="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
                 <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z"/>
